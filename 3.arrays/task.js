b0653a485e0850f5270783e866e54dd7de0a124a
Бухалова Елена <alenkavb@gmail.com>
2022-06-09 14:54:22 +0700

    solution

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -2,6 +2,7 @@ function compareArrays(arr1, arr2) {
   let result;
 
   // Ваш код
+  result = (arr1.length === arr2.length) && arr1.every((item, i) => arr2[i] === item);
 
   return result; // boolean
 }
@@ -10,6 +11,6 @@ function advancedFilter(arr) {
   let resultArr;
 
   // Ваш код
-
+  resultArr = arr.filter((item) => item > 0 && item % 3 === 0).map((item) => item * 10);
   return resultArr; // array
 }
