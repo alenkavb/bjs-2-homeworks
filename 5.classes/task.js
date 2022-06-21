745c4c6c740bb658254327f1ab5c551ad440a16e
Бухалова Елена <alenkavb@gmail.com>
2022-06-21 09:35:15 +0700

    solusion3

--- 5.classes/task.js
+++ 5.classes/task.js
@@ -105,22 +105,84 @@ class Library {
 }
 
 //  Задача №3
-
 class Student {
   constructor(name) {
     this.name = name;
-    this.subjects = {};
+    this.subjects = [];
   }
 
   addMark(grade, subName) {
     if (grade <= 0 || grade > 5) {
-      return ("Ошибка, оценка должна быть числом от 1 до 5");
+      return console.log("Ошибка, оценка должна быть числом от 1 до 5");
     }
 
-    if (this.subjects.isEmpty()) {
-      this.subjects.subjectName = subName;
-      this.subjects.grades = [grade];
+    if (isEmpty(this.subjects)) {
+      this.subjects.push({ subjectName: subName, grades: [grade] });
+
+    } else {
+      const obj = this.subjects.find(subject => subject.subjectName === subName);
+      if (obj === undefined) {
+        this.subjects.push({ subjectName: subName, grades: [grade] });
+      } else {
+        obj.grades.push(grade);
+      }
     }
+  }
+
+  getAverageBySubject(subName) {
+    const obj = this.subjects.find(subject => subject.subjectName === subName);
+    if (obj === undefined) {
+      return console.log("Несуществующий предмет");
+    }
+
+    const average = averageCalculation(obj.grades);
+    console.log(`Средний балл по предмету ${obj.subjectName} ${average}`);
+
+    return average;
+  }
+
+  getAverage() {
+    const averages = [];
+    this.subjects.forEach(element => {
+      const average = this.getAverageBySubject(element.subjectName);
+      averages.push(average);
+    });
+    const totalAverage = averageCalculation(averages);
+    console.log(`Средний балл по всем предметам ${totalAverage}`);
+    return totalAverage;
+  }
+
+  exclude(reason) {
+    this.excluded = reason;
+    delete (this.subjects);
   }
 }
 
+function isEmpty(obj) {
+  for (let key in obj) {
+    return false;
+  }
+  return true;
+}
+
+function averageCalculation(array) {
+  const average = array.reduce(function (acc, item, idx, arr) {
+    if (idx === arr.length - 1) {
+      return (acc + item) / arr.length;
+    } else {
+      return acc + item;
+    }
+  });
+  return average;
+}
+
+const student = new Student("Петр Иванов");
+student.addMark(5, "algebra");
+console.log(student);
+student.addMark(5, "algebra");
+student.addMark(5, "geometry");
+student.addMark(4, "geometry");
+student.getAverageBySubject("geometry");
+student.getAverageBySubject("biology");
+student.getAverage();
+student.exclude("Исключен за попытку подделать оценки");
